e8da30f13ad3832f7114b47a3793c1f45aafa3b0
glorianayeli <glorianayeli_jaime@ucol.mx>
2021-01-09 09:10:07 -0600

    apply the rules for the handshake in the array_binary and the reverse in the loop author: glorianayeli <glorianayeli_jaime@ucol.mx>

--- app/handshake.rb
+++ app/handshake.rb
@@ -2,6 +2,7 @@ class Handshake
     def initialize
         @numero_decimal = 0
         @array_binary = []
+        @handshake = []
     end
 
     def ask_number
@@ -9,19 +10,11 @@ class Handshake
         @numero_decimal = gets.chomp.to_i
     end
 
-    #def decimal_to_binary
-    #    @numero_decimal=@numero_decimal.to_s(2)
-    #    puts "#{@numero_decimal}"
-    #    binary_to_array
-    #end
-   
-
     def binary_to_array
         while @numero_decimal >= 1 do 
             (number_par(@numero_decimal)) ? @array_binary.unshift(0) : @array_binary.unshift(1)
             @numero_decimal = @numero_decimal / 2
         end
-        puts "#{@array_binary}"
     end
 
     def number_par(numero_decimal)
@@ -30,7 +23,24 @@ class Handshake
 
     def apply_handshake
         @array_binary.each_with_index do |number, index|
-            if (index == 1 and number == 1) ? 
+            @handshake << "wink" if (index == 0 && number == 1) 
+            @handshake << "double blink" if (index == 1 && number == 1) 
+            @handshake << "close your eyes" if (index == 2 && number == 1)
+            @handshake << "jump" if (index == 3 && number == 1) 
+            reverse_handshake if (index == 4 && number == 1)
+        end
+        puts "#{@handshake}"
+    end
+
+    def reverse_handshake
+        @handshake = []
+        @array_binary.each_with_index do |number, index|
+            @handshake.<< ("wink") if (index == 4 && number == 1) 
+            @handshake.<< ("double blink") if (index == 3 && number == 1) 
+            @handshake.<< ("close your eyes") if (index == 2 && number == 1)
+            @handshake.<< ("jump") if (index == 1 && number == 1)
         end
     end
+
+ 
 end
