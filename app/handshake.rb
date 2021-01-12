73cce7e337abb42cb9d829cdd99935c53621bb4e
glorianayeli <glorianayeli_jaime@ucol.mx>
2021-01-11 18:09:06 -0600

    add the sandimeter, rubocop

--- app/handshake.rb
+++ app/handshake.rb
@@ -1,46 +1,46 @@
+# frozen_string_literal: true
+
 class Handshake
-    def initialize
-        @numero_decimal = 0
-        @array_binary = []
-        @handshake = []
-    end
+  def initialize
+    @numero_decimal = 0
+    @array_binary = []
+    @handshake = []
+  end
 
-    def ask_number
-        puts "Ingresa numero decimal"
-        @numero_decimal = gets.chomp.to_i
-    end
+  def ask_number
+    puts 'Ingresa numero decimal'
+    @numero_decimal = gets.chomp.to_i
+  end
 
-    def binary_to_array
-        while @numero_decimal >= 1 do 
-            (number_par(@numero_decimal)) ? @array_binary.unshift(0) : @array_binary.unshift(1)
-            @numero_decimal = @numero_decimal / 2
-        end
+  def binary_to_array
+    while @numero_decimal >= 1
+      number_par(@numero_decimal) ? @array_binary.unshift(0) : @array_binary.unshift(1)
+      @numero_decimal /= 2
     end
+  end
 
-    def number_par(numero_decimal)
-        numero_decimal%2 == 0
+  def number_par(numero_decimal)
+    numero_decimal.even?
+  end
+
+  def apply_handshake
+    @array_binary.each_with_index do |number, index|
+      @handshake << 'wink' if index.zero? && number == 1
+      @handshake << 'double blink' if index == 1 && number == 1
+      @handshake << 'close your eyes' if index == 2 && number == 1
+      @handshake << 'jump' if index == 3 && number == 1
+      reverse_handshake if index == 4 && number == 1
     end
+    puts @handshake.to_s
+  end
 
-    def apply_handshake
-        @array_binary.each_with_index do |number, index|
-            @handshake << "wink" if (index == 0 && number == 1) 
-            @handshake << "double blink" if (index == 1 && number == 1) 
-            @handshake << "close your eyes" if (index == 2 && number == 1)
-            @handshake << "jump" if (index == 3 && number == 1) 
-            reverse_handshake if (index == 4 && number == 1)
-        end
-        puts "#{@handshake}"
+  def reverse_handshake
+    @handshake = []
+    @array_binary.each_with_index do |number, index|
+      @handshake.<< ('wink') if index == 4 && number == 1
+      @handshake.<< ('double blink') if index == 3 && number == 1
+      @handshake.<< ('close your eyes') if index == 2 && number == 1
+      @handshake.<< ('jump') if index == 1 && number == 1
     end
-
-    def reverse_handshake
-        @handshake = []
-        @array_binary.each_with_index do |number, index|
-            @handshake.<< ("wink") if (index == 4 && number == 1) 
-            @handshake.<< ("double blink") if (index == 3 && number == 1) 
-            @handshake.<< ("close your eyes") if (index == 2 && number == 1)
-            @handshake.<< ("jump") if (index == 1 && number == 1)
-        end
-    end
-
- 
+  end
 end
--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'handshake'
 @game = Handshake.new
 @game.ask_number
